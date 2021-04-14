5b98ba87329adc3daaae773b774a090fbc729dd9
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2021-04-14 11:03:40 +0300

    Added NavLink to dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,34 +1,35 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-            <div className={s.dialogs}>
-                <div className={s.dialogsItems}>
-                    <div className={s.dialog + ' ' + s.active}>
-                        Sasha
-                    </div>
-                    <div className={s.dialog}>
-                        Misha
-                    </div>
-                    <div className={s.dialog}>
-                        Vasya
-                    </div>
-                    <div className={s.dialog}>
-                        Bodya
-                    </div>
-                    <div className={s.dialog}>
-                        Roma
-                    </div>
-                    <div className={s.dialog}>
-                        Vanya
-                    </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Sasha</NavLink>
                 </div>
-                <div className={s.messages}>
-                    <div className={s.message}>Hello</div>
-                    <div className={s.message}>How are you?</div>
-                    <div className={s.message}>Chao</div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Misha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Vasya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Bodya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Roma</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6'>Vanya</NavLink>
                 </div>
             </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Chao</div>
+            </div>
+        </div>
     )
 }
 
