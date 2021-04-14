ccd33981cc435365d2f2e3a27ce43ee2e6c67585
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2021-04-14 10:43:13 +0300

    Added dialog and message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,33 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
-        </div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Sasha
+                    </div>
+                    <div className={s.dialog}>
+                        Misha
+                    </div>
+                    <div className={s.dialog}>
+                        Vasya
+                    </div>
+                    <div className={s.dialog}>
+                        Bodya
+                    </div>
+                    <div className={s.dialog}>
+                        Roma
+                    </div>
+                    <div className={s.dialog}>
+                        Vanya
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hello</div>
+                    <div className={s.message}>How are you?</div>
+                    <div className={s.message}>Chao</div>
+                </div>
+            </div>
     )
 }
 
